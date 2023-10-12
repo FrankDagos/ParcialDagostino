72024dca5c1fec63b1beb1816534e95709554064
Admin <francodagostino2008@hotmail.com>
2023-10-11 22:28:47 -0300

    Initial commit

---
+++ src/main/java/com/example/demo/services/BaseService.java
@@ -0,0 +1,29 @@
+package com.example.demo.services;
+
+import com.example.demo.entities.Base;
+import com.example.demo.repositories.BaseRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface BaseService<E extends Base, ID extends Serializable> {
+
+    // Trae una lista de todas las personas (entidades) que se encuentran en la base de datos
+    public List<E> findALL() throws Exception;
+
+    public Page<E> findALL(Pageable pageable) throws Exception;
+
+    // Trae una entidad de acuerdo a un id que le pasemos
+    public E findById(ID id) throws Exception;
+
+    // Crea una nueva entidad y para eso le vamos a enviar una nueva entidad al repositorio
+    public E save(E entity) throws Exception;
+
+    // Tener un id y una entidad, la entidad actualizada y el id de la entidad que se quiere actualizar
+    public E update(ID id, E entity) throws Exception;
+
+    // Se encarga de eliminar el registro de la base de datos
+    public boolean delete(ID id) throws Exception;
+}
